46a8965e86a59fc5ede4fcaae997db4c0c57786a
IstvanPongracz <pongracz88@gmail.com>
2022-05-26 10:21:08 +0200

    refactoring, test

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const { logger } = require("./middleware/logger")
+const auth = require("./middleware/auth")
+const { errorHandler } = require("./middleware/errorHandler")
+
+const app = express()
+
+
+const dashboard = require("./route/dashboard")
+
+
+app.use(
+  cors({
+      origin: process.env.APP_URL
+  })
+);
+
+app.use(express.json())
+
+app.use(logger);
+
+app.use('/api/dashboards', dashboard);
+
+
+app.get("/api/public", (req, res) => {
+  console.log("public")
+  res.send("Hello public")
+})
+app.get("/api/private", auth({block: true}), (req, res) => {
+  console.log('private')
+  res.send(`Hello private id: ${res.locals.userId}`)
+  
+})
+
+app.get("/api/anonymus", auth({block: false}), (req , res) => {
+  if (!res.locals.userId) return res.send("Hello public") 
+  res.send(`Hello anonymus id: ${res.locals.userId}`)
+})
+
+// const myBusinessLogic = (req, res) => {
+//     if (!res.locals.userId) return res.sendStatus(401)
+//     console.log("My business logic runs");
+//     res.status(200).json("Success");
+// }
+
+app.use(errorHandler)
+
+module.exports = app
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 
-exports.auth = (middlewareParams) => (req, res, next) => {
+const auth = (middlewareParams) => (req, res, next) => {
         console.log("Authentication");
         // let a;
         // console.log(a.b.c)
@@ -8,3 +8,5 @@ exports.auth = (middlewareParams) => (req, res, next) => {
         if (middlewareParams.block && !res.locals.userId) return res.sendStatus(401)
         next();
     }
+
+    module.exports = auth
