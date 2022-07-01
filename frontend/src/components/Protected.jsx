c0edc5216b25b4b150cb17ccd381f4d58e9c6f53
IstvanPongracz <pongracz88@gmail.com>
2022-07-01 19:01:15 +0200

    Protected modified

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,21 +1,22 @@
 import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../providers/auth';
 
 function Protected ({children}) {
     const {token, user} = useAuth();
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    // useEffect(() => {
-    //   console.log(token, "Hol vagyunk?");
-    //     if (!token) {
-    //       return navigate('/');
-    //     }
-    //     if (!user.userId) {
-    //       return navigate('/register')
-    //     }
-    //     // eslint-disable-next-line
-    //   }, [token, user])
+    useEffect(() => {
+        if (!token) {
+          return navigate('/');
+        }
+        if (!user.userId) {
+          return navigate('/register')
+        }
+        // eslint-disable-next-line
+      }, [token, user]);
+      
   return (
     // <React.Fragment>{children}</React.Fragment>
     <>
